e83a084a9edb116f4b54f788ece0e342885dee1d
KarlssonBernoulli <78110504+KarlssonBernoulli@users.noreply.github.com>
2021-01-27 21:46:58 +0100

    Update temp.cs

--- temp.cs
+++ temp.cs
@@ -1,4 +1,17 @@
- public class MyProjection
+public class Car
+    {
+        public long Id {get;set;}
+        public string Name {get;set;}
+        public DateTime DateTime {get;set;}
+        public bool IsSet {get;set;}
+        public long GroupId {get; set;}
+        public Group Group {get; set;}
+
+        public ICollection<CarTyreMapping> TyreHistoryMappings {get;set;}
+    } 
+
+
+public class MyProjection
     {
         public Car Car { get; set; }
         public Tyre Tyre { get; set; }
@@ -25,39 +38,20 @@
         {
             var context = new DataConnection(new SQLiteDataProvider(ProviderName.SQLiteClassic), "Data Source=:memory:");
             context.CreateTable<Car>();
-            context.CreateTable<Tyre>();
-            context.CreateTable<Group>();
-            context.CreateTable<CarTyreMapping>();
-            context.CreateTable<Brand>();
+
 
             var fluentMappingBuilder = context.MappingSchema.GetFluentMappingBuilder();
 
-            var brandBuilder = fluentMappingBuilder.Entity<Brand>();
-            brandBuilder.Property(x => x.Id).IsPrimaryKey();
-            brandBuilder.Association(x => x.Groups, x => x.Id, x => x.BrandId);
-
-            var groupBuilder = fluentMappingBuilder.Entity<Group>();
-            groupBuilder.Property(x => x.Id).IsPrimaryKey();
-            groupBuilder.Association(x => x.Cars, x => x.Id, x => x.GroupId);
-            groupBuilder.Association(x => x.Brand, x => x.BrandId, x => x.Id);
 
             var carBuilder = fluentMappingBuilder.Entity<Car>();
             carBuilder.Property(x => x.Id).IsPrimaryKey();
             carBuilder.Association(x => x.Group, x => x.GroupId, x => x.Id);
 
-            var tyreHistoryMappingBuilder = fluentMappingBuilder.Entity<CarTyreMapping>();
-
-            var tyreBuilder = fluentMappingBuilder.Entity<Tyre>();
-            tyreBuilder.Property(x => x.Id).IsPrimaryKey();
 
             context.Insert(new Car { Id = 1, Name = "MyCar", IsSet = true });
-            context.Insert(new Car { Id = 2, Name = "Bla" });
-            context.Insert(new Group { Id = 1, Name = "group 1", IsSet = false });
-            context.Insert(new Tyre { Id = 1, Name = "MyTyre", CarId = 1 });
-            context.Insert(new CarTyreMapping { CarId = 1, TyreId = 1, Time = DateTime.UtcNow.AddDays(-2) });
+
 
             var carTable = context.GetTable<Car>();
-            var tyreTable = context.GetTable<Tyre>();
 
 
             var query = carTable.Where(x => x.FilterBySpecialString(null)).ToList();
